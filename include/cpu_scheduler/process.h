a6346544f52b26878965eededebe6d4e2919091b
Nithwin <vmnithwin@gmail.com>
2026-07-20 16:19:09 +0530

    feat: implement getters and setters for the Process class attributes

--- include/cpu_scheduler/process.h
+++ include/cpu_scheduler/process.h
@@ -11,16 +11,38 @@ enum class ProcessState
 };
 
 // Process - Represents a process in the operating system
-class Process {
+class Process 
+{
     private:
+        // Identity
         const int pid; // pid - process id
         const int arrivalTime; // arrivalTime - time at which process arrives
         const int burstTime; // burstTime - time required to complete the process
         const int priority; // priority - priority of the process
+
+        // Runtime
         int remainingTime; // remainingTime - time remaining for the process to complete
         ProcessState state; // state - current state of the process
         int startTime; // startTime - time at which process starts
         int completionTime; // completionTime - time at which process completes
     public:
         Process(int pid, int arrivalTime, int burstTime, int priority);
+
+        // Getters
+        int getPid() const;
+        int getArrivalTime() const;
+        int getBurstTime() const;
+        int getPriority() const;
+
+        int getRemainingTime() const;
+        ProcessState getState() const;
+        int getStartTime() const;
+        int getCompletionTime() const;
+
+        // Setters
+        void setRemainingTime(int remainingTime) ;
+        void setState(ProcessState state) ;
+        void setStartTime(int startTime) ;
+        void setCompletionTime(int completionTime) ;
+
 };
--- src/cpu_scheduler/process.cpp
+++ src/cpu_scheduler/process.cpp
@@ -6,10 +6,71 @@ Process::Process(
     arrivalTime(arrivalTime),
     burstTime(burstTime),
     priority(priority),
-    ProcessState(ProcessState::New),
     remainingTime(burstTime),
+    state(ProcessState::New),
     startTime(-1),
-    completionTime(-1),
+    completionTime(-1)
     {
         
     }
+
+// Getters Functions
+int Process::getPid() const 
+{
+    return pid;
+}
+
+int Process::getArrivalTime() const 
+{
+    return arrivalTime;
+}
+
+int Process::getBurstTime() const 
+{
+    return burstTime;
+}
+
+int Process::getPriority() const 
+{
+    return priority;
+}
+
+int Process::getRemainingTime() const 
+{
+    return remainingTime;
+}
+
+ProcessState Process::getState() const 
+{
+    return state;
+}
+
+int Process::getStartTime() const 
+{
+    return startTime;
+}
+
+int Process::getCompletionTime() const 
+{
+    return completionTime;
+}
+
+void Process::setRemainingTime(int remainingTime)  
+{
+    this->remainingTime = remainingTime;
+}
+
+void Process::setCompletionTime(int completionTime)  
+{
+    this->completionTime = completionTime;
+}
+
+void Process::setStartTime(int startTime)  
+{
+    this->startTime = startTime;
+}
+
+void Process::setState(ProcessState state)  
+{
+    this->state = state;
+}
