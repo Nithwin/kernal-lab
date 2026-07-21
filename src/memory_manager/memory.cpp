661df055f560ba13280072cf46c9f3682a1e2673
Nithwin <vmnithwin@gmail.com>
2026-07-21 14:40:12 +0530

    feat: implement Memory management class with getters and setters

---
+++ src/memory_manager/memory.cpp
@@ -0,0 +1,45 @@
+#include "memory_manager/memory.h"
+
+Memory::Memory(
+    int startAddress, int size
+) : startAddress(startAddress),
+    size(size),
+    allocated(false),
+    pid(-1)
+
+{
+
+}
+
+
+int Memory::getStartAddress() const
+{
+    return startAddress;
+}
+
+int Memory::getSize() const
+{
+    return size;
+}
+
+bool Memory::getIsAllocated() const
+{
+    return allocated;
+}
+
+int Memory::getPid() const
+{
+    return pid;
+}
+
+// Setters 
+
+void Memory::setIsAllocated(bool allocated)
+{
+    this->allocated = allocated;
+}
+
+void Memory::setPid(int pid)
+{
+    this->pid = pid;
+}
