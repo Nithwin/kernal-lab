98ef57784bcde1820139da82c81f0f238dc91bf5
Nithwin <vmnithwin@gmail.com>
2026-07-24 22:06:12 +0530

    feat: add thread-safe mutex implementation to demo and introduce semaphore synchronization example

--- src/synchronization/mutex_demo.cpp
+++ src/synchronization/mutex_demo.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+
+std::mutex mutexCounter;
 
 int counter = 0;
 
@@ -7,7 +10,10 @@ void increment()
 {
     for (int i = 0; i < 100000; i++)
     {
+        std::lock_guard<std::mutex> lock(mutexCounter);
+
         counter++;
+
     }
 }
 
